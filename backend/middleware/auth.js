94ca2ca1bdf6604ab553bbdd14d16634a129f5e9
davecoders0201 <davecoder0201@gmail.com>
2023-03-27 13:04:39 +0530

    Finished

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  var token = req.headers['token'];
+  var token = req.headers["token"];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
-  }
-  else
-  {
+  } else {
     const decoded = jwt.verify(token, "RANDOM-TOKEN");
-  console.log("requested body",decoded)
-     req.user = decoded;
-   
+    console.log("requested body", decoded);
+    req.user = decoded;
   }
-  
+
   return next();
 };
 
--- backend/routes/ecommerce.js
+++ backend/routes/ecommerce.js
@@ -1,27 +1,18 @@
 var express = require("express");
 var multer = require("multer");
 var router = express.Router();
+const auth = require("../middleware/auth");
 const registerEndPoints = require("../controller/register");
 const loginEndPoints = require("../controller/login");
 const userList = require("../controller/userList");
 const getSingle = require("../controller/getSingle");
 const deleteUser = require("../controller/deleteUser");
 const editUser = require("../controller/editUser");
-// const cartData = require("../controller/cartData");
-// const cartDetails = require("../controller/cartDetails");
 const productData = require("../controller/products/productData");
 const productDetails = require("../controller/products/productDetails");
-// const tabletData = require("../controller/tabletData");
-// const watchData = require("../controller/watchData");
-// const watchDetails = require("../controller/watchDetails");
-// const monitorData = require("../controller/monitorData");
-// const monitorDetails = require("../controller/monitorDetails");
-
-const auth = require("../middleware/auth");
 const productEdit = require("../controller/products/productEdit");
 const productgetSingle = require("../controller/products/productGetSingle");
 const productDelete = require("../controller/products/productDelete");
-// const tabletDetails = require("../controller/tabletDetails");
 
 var upload = multer({ dest: "../uploads/" });
 
@@ -44,7 +35,6 @@ router.get("/get_all_user", userList);
 router.get("/getSingle", getSingle);
 router.delete("/deleteUser", deleteUser);
 router.put("/editUser", editUser);
-
 router.post("/productData", upload.single("image"), productData);
 router.get("/productDetails", productDetails);
 router.put("/productEdit", upload.single("image"), productEdit);
