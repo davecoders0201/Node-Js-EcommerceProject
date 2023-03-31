9352d86ff3e285effe9aa47954dc1a7dc5bcf744
davecoders0201 <davecoder0201@gmail.com>
2023-03-31 13:43:01 +0530

    some changes

--- backend/controller/products/productData.js
+++ backend/controller/products/productData.js
@@ -1,15 +1,14 @@
 const express = require("express");
 var multer = require("multer");
 const ProductModel = require("../../models/productModel");
-
 const app = express();
 
 const productData = function (request, response) {
   const product = new ProductModel({
     title: request.body.title,
     price: request.body.price,
-    image: process.env.BACKEND_URL + request.file.path,
-    description: request.body.description,
+    image: process.env.REACT_APP_API_KEY + request.file.path,
+    // description: request.body.description,
     rating: request.body.rating,
   });
   product
